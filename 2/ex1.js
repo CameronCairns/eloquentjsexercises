0613990ca40a6126ddd7625484cfba29550f81a6
Cameron Cairns <cameroncairns@gmail.com>
2015-09-06 17:51:37 -0700

    First exercise successfully completed

---
+++ 2/ex1.js
@@ -0,0 +1,7 @@
+//Writted by Cameron Cairns
+//Exercise 2.1 Looping a Triangle
+var triangleString = ''
+while (triangleString.length < 7){
+    triangleString += '#'
+    console.log(triangleString)
+}
