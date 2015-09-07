841d120d5bc62666f46f551135060cd42db82c40
Cameron Cairns <cameroncairns@gmail.com>
2015-09-06 18:01:52 -0700

    Added some comments to exercise 2

--- 2/ex2.js
+++ 2/ex2.js
@@ -1,6 +1,16 @@
 //Written by Cameron Cairns
 //Exercise 2.2 FizzBuzz
 for(var count = 1; count <= 100; count++){
+    //Note that this is defined first
+    //so that fizzbuzz numbers won't 
+    //be caught by the logic of 
+    //mod 3 or 5 since it satisfies
+    //that as well
+    //Note that the lowest number
+    //divisible by two factors
+    //is the factors multiplied
+    //by one another in  this 
+    //case 15
     if(count % 15 === 0){
         console.log('FizzBuzz')
     }
